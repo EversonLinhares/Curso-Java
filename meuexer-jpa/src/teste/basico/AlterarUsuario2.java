c14d6ab0d4abbb1ae1acf06c0b504a5f702a12a2
EversonLinhares <68698121+EversonLinhares@users.noreply.github.com>
2021-04-27 08:14:39 -0300

    Add files via upload

---
+++ meuexer-jpa/src/teste/basico/AlterarUsuario2.java
@@ -0,0 +1,31 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class AlterarUsuario2 {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence
+	    		  .createEntityManagerFactory("meuexer-jpa");
+	      EntityManager em = emf.createEntityManager();
+	      
+	      em.getTransaction().begin();
+	      
+	      Usuario usuario = em.find(Usuario.class, 7L);
+	      usuario.setNome("Everson");
+	      
+	      //Está alterando o objeto sem o merge porque o objeto está em estado gerenciado
+	      //pelo gettransaction
+	      //em.merge(usuario);
+	      
+	      
+	      em.getTransaction().commit();
+	      
+	      em.close();
+	      emf.close();
+	}
+}
---
+++ meuexer-jpa/src/teste/basico/RemoverUsuario.java
@@ -0,0 +1,28 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class RemoverUsuario {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence
+	    		  .createEntityManagerFactory("meuexer-jpa");
+	      EntityManager em = emf.createEntityManager();
+	      
+	      Usuario usuario = em.find(Usuario.class, 8L);
+	      System.out.println(usuario.getNome());
+	      if(usuario != null) {
+	    	  em.getTransaction().begin();
+	    	  em.remove(usuario);
+	    	  em.getTransaction().commit();
+	      }
+	      
+	      
+	      em.close();
+	      emf.close();
+	}
+}
